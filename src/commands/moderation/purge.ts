bb084e9b08c4d458b8306afc8b534a1f1d4e337a
Ben <benlaugherty@gmail.com>
2020-05-18 15:54:53 -0600

    WIP larger purge, and a cleaned up normal purge

--- src/commands/moderation/purge.ts
+++ src/commands/moderation/purge.ts
@@ -25,13 +25,49 @@ export default class PurgeCommand extends Command {
     }
 
     public async exec(msg: Message, { amount }: { amount: number }) {
+        // TODO: Update this to remove max 1k msgs.
         if (!amount) {
             return msg.util?.send(
-                'Please specify an amount of messages to purge.'
+                'Please specify an amount of messages to purge. (Max 100)'
             );
         }
 
-        msg.channel.bulkDelete(amount + 1);
-        return msg.util?.send(`Deleted \`${amount}\` messages.`);
+        // delete the first message so we do not need to deal with the math...
+        await msg.delete();
+
+        // if its less than 100 messages, just call a bulk delete on those
+        if (amount < 100) {
+            let msgs = await msg.channel.bulkDelete(amount, true);
+            return msg.util?.send(
+                `Deleted \`${msgs.size}\` message(s).${
+                    msgs.size < amount
+                        ? ' Less messages were probably purged due to messages being older than 2 weeks or other discord limitation.'
+                        : ''
+                }`
+            );
+        } else {
+            return msg.util?.send('Please specify an amount under 100.');
+        }
+
+        // let total = 0;
+        // while (amount >= 101) {
+        //     let amt = Math.min(100, amount - total);
+        //     let msgs = await msg.channel.bulkDelete(amt, true);
+        //
+        //     if (amt !== msgs.size) {
+        //         total += msgs.size;
+        //         return msg.util?.send(
+        //             `Deleted \`${total}\` messages. Less messages were probably purged due to messages being older than 2 weeks or other discord limitation.`
+        //         );
+        //     }
+        //
+        //     amount -= msgs.size;
+        //     total += msgs.size;
+        // }
+        //
+        // await msg.channel.bulkDelete(amount, true).then((msgs) => {
+        //     amount += msgs.size;
+        //     return msg.util?.send(`Deleted \`${total}\` messages.`);
+        // });
     }
 }
