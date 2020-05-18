5252e5be8efa02633ec2c199c6c80401d6ec5f6e
Ben <benlaugherty@gmail.com>
2020-05-18 17:00:51 -0600

    update 'watching' status with servers/member

--- src/listeners/client/ready.ts
+++ src/listeners/client/ready.ts
@@ -11,5 +11,13 @@ export default class ReadyListener extends Listener {
 
     public async exec() {
         console.log(`${this.client.user.tag} is now online.`);
+
+        // Update servers/members every 5 minutes.
+        setInterval(() => {
+            this.client.user.setActivity(
+                `${this.client.guilds.cache.size} servers | ${this.client.users.cache.size} members`,
+                { type: 'WATCHING' }
+            );
+        }, 300000);
     }
 }
