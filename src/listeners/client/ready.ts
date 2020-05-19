602c6cc358d11c9c43567fd733d348f886fabb25
Ben <benlaugherty@gmail.com>
2020-05-19 02:06:58 -0600

    user will still be un-muted even if bot restarts

--- src/listeners/client/ready.ts
+++ src/listeners/client/ready.ts
@@ -1,4 +1,6 @@
 import { Listener } from 'discord-akairo';
+import { Mutes } from '../../models/mutes';
+import { Servers } from '../../models/server';
 
 export default class ReadyListener extends Listener {
     public constructor() {
@@ -10,7 +12,11 @@ export default class ReadyListener extends Listener {
     }
 
     public async exec() {
+        // Log bot is online :)
         console.log(`${this.client.user.tag} is now online.`);
+
+        const mutesRepo = this.client.db.getRepository(Mutes);
+        const serversRepo = this.client.db.getRepository(Servers);
 
         // Update servers/members every 5 minutes.
         this.client.user.setActivity(
@@ -23,6 +29,24 @@ export default class ReadyListener extends Listener {
                 `${this.client.guilds.cache.size} servers | ${this.client.users.cache.size} members`,
                 { type: 'WATCHING' }
             );
-        }, 300000);
+        }, 3e5);
+
+        setInterval(async () => {
+            const mutes = await mutesRepo.find();
+            mutes
+                .filter((m) => m.end <= Date.now())
+                .map(async (m) => {
+                    let guild = this.client.guilds.cache.get(m.server);
+                    let member = guild.members.cache.get(m.user);
+                    // TODO: This is slow. Optimize with relations or something? :)
+                    let serverDb = await serversRepo.findOne({
+                        where: { server: m.server },
+                    });
+
+                    // Remove the mute
+                    // TODO: Replace this with 'unmute' function that we will create
+                    await member.roles.remove(serverDb.mutedRole, 'Unmuted');
+                });
+        }, 30000);
     }
 }
