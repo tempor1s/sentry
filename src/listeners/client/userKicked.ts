14466ce0c1ce03f9935a2a9bfa01944d79ef8e0b
Ben <benlaugherty@gmail.com>
2020-05-26 01:35:46 -0600

    add manual user kick logging

---
+++ src/listeners/client/userKicked.ts
@@ -0,0 +1,38 @@
+import { Listener } from 'discord-akairo';
+import { GuildMember } from 'discord.js';
+import { Servers } from '../../models/server';
+import { Repository } from 'typeorm';
+import { logKick } from '../../structures/logManager';
+
+export default class UserKickedListener extends Listener {
+  public constructor() {
+    super('userKickedListener', {
+      emitter: 'client',
+      event: 'guildMemberRemove',
+    });
+  }
+
+  public async exec(member: GuildMember) {
+    const serversRepo: Repository<Servers> = this.client.db.getRepository(
+      Servers
+    );
+
+    let auditLogs = await member.guild.fetchAuditLogs();
+    // TODO: This might fail. Maybe sort through the first 1-10 and find the EXACT action? :)
+    let possibleKickAction = auditLogs.entries.first();
+
+    // ignore if not a kick event
+    if (possibleKickAction.action !== 'MEMBER_KICK') return;
+    // dont log actions that are done by the bot because it will already be logged through the kick action
+    if (possibleKickAction.executor.id == this.client.user.id) return;
+
+    logKick(
+      serversRepo,
+      member,
+      possibleKickAction.reason
+        ? possibleKickAction.reason
+        : 'No reason provided.',
+      member.guild.members.cache.get(possibleKickAction.executor.id)
+    );
+  }
+}
