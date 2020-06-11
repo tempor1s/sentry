6fbfac169deed3b82a4eb966165f01a1c0bdf51c
Ben <benlaugherty@gmail.com>
2020-06-10 19:01:54 -0600

    remove more unused code

--- frontend/server/configureClient.ts
+++ frontend/server/configureClient.ts
@@ -1,11 +1,9 @@
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink, concat } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { getMainDefinition } from 'apollo-utilities';
 import withApollo from 'next-with-apollo';
 import { HttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import { SERVER } from './config';
 
 // interface Definiton {
@@ -13,49 +11,49 @@ import { SERVER } from './config';
 //   operation?: string;
 // }
 
-let authToken: string | null = null;
+// let authToken: string | null = null;
+
+// const authMiddleware = new ApolloLink((operation, forward) => {
+//   operation.setContext({
+//     headers: {
+//       authorization: authToken || null,
+//     },
+//   });
+//
+//   return forward(operation);
+// });
+//
+// export const setToken = async (token: string) => {
+//   try {
+//     authToken = token ? `Bearer ${token}` : null;
+//     Cookies.set('token', authToken!, { expires: 7 });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+//
+// export const setTokenInRequest = async (token: string) => {
+//   try {
+//     authToken = token ? token : null;
+//     return authToken;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+//
+// export const destroyToken = async (token: string) => {
+//   try {
+//     Cookies.remove('token');
+//     authToken = null;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 const httpLink = new HttpLink({
   fetch,
   uri: SERVER,
 });
-
-const authMiddleware = new ApolloLink((operation, forward) => {
-  operation.setContext({
-    headers: {
-      authorization: authToken || null,
-    },
-  });
-
-  return forward(operation);
-});
-
-export const setToken = async (token: string) => {
-  try {
-    authToken = token ? `Bearer ${token}` : null;
-    Cookies.set('token', authToken!, { expires: 7 });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const setTokenInRequest = async (token: string) => {
-  try {
-    authToken = token ? token : null;
-    return authToken;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const destroyToken = async (token: string) => {
-  try {
-    Cookies.remove('token');
-    authToken = null;
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 export default withApollo(
   ({ initialState }) =>
